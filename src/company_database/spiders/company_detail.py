62c2b5a080c3f93a85fb23602b4bcf2b494fdd26
ashikshezan <ashikshezan@gmail.com>
2021-01-21 17:40:31 +0600

    company_profile.json done

--- src/company_database/spiders/company_detail.py
+++ src/company_database/spiders/company_detail.py
@@ -1,7 +1,10 @@
+from os import path
 import scrapy
 import json
-from urllib.parse import urljoin
-from os import path
+from urllib.parse import urljoin, urlparse
+from typing import Dict, List
+
+from .helper_tools import get_company_profile, get_company_contacts
 
 BASE_DIR = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
 
@@ -10,10 +13,11 @@ class CompanyDetailSpider(scrapy.Spider):
     name = 'company_detail'
     allowed_domains = ['www.adapt.io']
 
-    handle_httpstatus_list = [200, 410]
+    handle_httpstatus_list = [200, 410, 503]
 
     # Some custom variables for debugging purposes
     http_410 = []
+    http_503_count = 0
 
     def __init__(self):
         self.company_index_list = []
@@ -33,8 +37,10 @@ class CompanyDetailSpider(scrapy.Spider):
                 "'company_index.json' file is needed to run the spider")
 
     def __del__(self):
+        print('\n\n\n')
         print(
             f'{len(self.company_index_list)} companies still to be crawled ________________')
+        print(f'HTTP 503 retuned: {self.http_503_count} ______________')
         print(f'HTTP 410 retuned: {len(self.http_410)} ______________')
 
         # Updating the company_index.json file by excluding the company that info
@@ -44,7 +50,7 @@ class CompanyDetailSpider(scrapy.Spider):
                 file.write(json.dumps(self.company_index_list))
 
         # Creating a new file for the companty index that returned HTTP 410
-        with open(self.company_index_of_http410_path, 'w') as file:
+        with open(self.company_index_of_http410_path, 'a') as file:
             file.write(json.dumps(self.http_410))
 
     def start_requests(self):
@@ -57,47 +63,28 @@ class CompanyDetailSpider(scrapy.Spider):
         A method to keep up-to-date the company urls list by excluding
         the urls that returned response successfully from self.company_index_list.
         Finally, at the end the self.company_index_list remains with the company urls list that
-        the spider could not scrap because of HTTP 503 error. 
+        the spider could not scrap because of HTTP 503 error.
         '''
         company_index = to_be_excluded_url.split(
             "https://www.adapt.io")[1]
         self.company_index_list = [
             i for i in self.company_index_list if not i['source_url'] == company_index]
 
-    def catch_410_error_list(self, url):
-        error_company_index = url.split(
-            "https://www.adapt.io")[1]
-        self.http_410.append(error_company_index)
-
     def parse(self, response):
-
         # just looking up the number http response I am getting
         if response.status == 410:
-            self.catch_410_error_list(response.url)
+            self.http_410.append(response.url)
             self.update_company_index_list(response.url)
 
         elif response.status == 200:
             self.update_company_index_list(response.url)
-            company_name = response.xpath(
-                "//div[@class='info-wrapper']//h1//text()").get()
-            location = response.xpath(
-                "//div[@class='info-wrapper']//li//span[text()='Location']//following-sibling::node()[2]/text()").get()
-            web_site = response.xpath(
-                "//div[@class='info-wrapper']//span[@class='website-url']/text()").get()
-            web_domain = web_site
-            industry_name = response.xpath(
-                "//div[@class='info-wrapper']//li//span[text()='Industry']//following-sibling::node()[2]/text()").get()
-            employee_size = response.xpath(
-                "//div[@class='info-wrapper']//li//span[text()='Head Count']//following-sibling::node()[2]/text()").get()
-            revenue = response.xpath(
-                "//div[@class='info-wrapper']//li//span[text()='Revenue']//following-sibling::node()[2]/text()").get()
 
-            yield {
-                "company name": company_name,
-                "company_location": location,
-                "company_website": web_site,
-                "company_webdomain": web_domain,
-                "company_industry": industry_name,
-                "company_employee_size": employee_size,
-                "company_revenue": revenue,
-            }
+            company_profile = get_company_profile(response)
+            company_contact_list = get_company_contacts(response)
+
+            company_profile['contact_details'] = company_contact_list
+
+            yield company_profile
+
+        elif response.status == 503:
+            self.http_503_count += 1
---
+++ src/company_database/spiders/helper_tools.py
@@ -0,0 +1,72 @@
+from typing import Dict, List
+from urllib.parse import urljoin, urlparse
+
+
+def get_company_profile(response) -> Dict:
+    company_name = response.xpath(
+        "//div[@class='info-wrapper']//h1//text()").get()
+    location = response.xpath(
+        "//div[@class='info-wrapper']//li//span[text()='Location']//following-sibling::node()[2]/text()").get()
+    web_site = response.xpath(
+        "//div[@class='info-wrapper']//span[@class='website-url']/text()").get()
+    web_domain = web_site
+    industry_name = response.xpath(
+        "//div[@class='info-wrapper']//li//span[text()='Industry']//following-sibling::node()[2]/text()").get()
+    employee_size = response.xpath(
+        "//div[@class='info-wrapper']//li//span[text()='Head Count']//following-sibling::node()[2]/text()").get()
+    revenue = response.xpath(
+        "//div[@class='info-wrapper']//li//span[text()='Revenue']//following-sibling::node()[2]/text()").get()
+    # extracting the company web domain name from the website url
+    if web_site:
+        web_domain = company_web_domain_exptractor(
+            web_site)
+    return {
+        "company_name": company_name,
+        "company_location": location,
+        "company_website": web_site,
+        "company_webdomain": web_domain,
+        "company_industry": industry_name,
+        "company_employee_size": employee_size,
+        "company_revenue": revenue,
+    }
+
+
+def get_company_contacts(response) -> List:
+    contact_list = []
+    contact_items = response.xpath(
+        "//div[@class='top-contact-item']")
+    for contact in contact_items:
+        print('==============')
+        name = contact.xpath('.//a//text()').get()
+        link = contact.xpath('.//a//@href').get()
+        job_title = contact.xpath(
+            ".//p[@class='contact-role']//text()").get()
+        email_domain = contact.xpath(
+            ".//span[@itemprop='email']//text()").get().strip()
+        # extracting the domain name only
+        email_domain = contact_email_domain_exptractor(
+            email_domain)
+        contact_list.append(
+            {
+                "contact_name": name,
+                "contact_jobtitle": job_title,
+                "contact_email_domain": email_domain,
+                "contact_profile_link": link,
+            }
+        )
+    return contact_list
+
+
+def contact_email_domain_exptractor(email: str) -> str:
+    return email.split('@')[-1]
+
+
+def company_web_domain_exptractor(website: str) -> str:
+    domain = urlparse(website).netloc
+    domain_parts = domain.split('.')
+    domain_name = '.'.join(domain_parts[1:])
+    return domain_name
+
+
+if __name__ == "__main__":
+    pass
